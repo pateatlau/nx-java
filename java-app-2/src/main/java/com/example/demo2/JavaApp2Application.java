81ec94356bd5ce8087d562a42e1bf2604c4a2262
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-06 14:12:57 +0530

    Second Java app created

---
+++ java-app-2/src/main/java/com/example/demo2/JavaApp2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApp2Application.class, args);
+		System.out.println("Hello Java2");
+	}
+
+}
